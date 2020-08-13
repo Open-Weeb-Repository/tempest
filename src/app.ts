0edebe3dd8b8992394f20da19f8af60a28c21a24
aquaswim <bayusetiawan37@gmail.com>
2020-08-13 22:32:23 +0700

    creating base app

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import debug from "debug";
+
+const log = debug('tempest:app');
+
+export class App {
+    constructor() {
+        log('Creating app instance');
+    }
+
+    async start(){
+
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
-console.log('hello world');
+import yargs from "yargs";
+import {App} from "./app";
+import {CronJob} from "cron";
+import debug from 'debug';
+
+const log = debug('tempest:main');
+
+yargs
+    .command('start [crontime]', 'Start the scrapper using on cron format', yargs => {
+        yargs.positional('crontime', {
+            describe: 'Cron format (* * * * * *)',
+            default: '0 0 0 * * 0'
+        })
+        yargs.option('run-oninit', {
+            type: "boolean",
+            description: "Run on init"
+        })
+    }, start)
+    .command('once', 'Start the scrapper only once', () => {
+    }, once)
+    .argv;
+
+interface IBaseArgs{}
+
+interface IStartArgs extends IBaseArgs{
+    crontime: string,
+    runOninit?: boolean
+}
+
+function start(argv: IStartArgs) {
+    log('creating cronjob');
+    new CronJob({
+        cronTime: argv.crontime,
+        onTick: ()=>once(),
+        runOnInit: argv.runOninit,
+        start: true
+    });
+}
+
+function once() {
+    log('Start process');
+    const app = new App();
+    return app.start()
+        .then(()=>{
+            console.log((new Date()).toISOString()+'::Process Done');
+        }).catch(err => {
+            console.error((new Date()).toISOString()+'::Process Error');
+            console.error(err);
+        });
+}
