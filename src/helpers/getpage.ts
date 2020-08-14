bba96c6c1821699855b5415ee236fe41d68ebc65
aquaswim <bayusetiawan37@gmail.com>
2020-08-14 19:45:43 +0700

    add encode url before getting page

--- src/helpers/getpage.ts
+++ src/helpers/getpage.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 
 export default async function (url: string): Promise<CheerioStatic> {
-    const body = await axios.get<string>(url);
+    const body = await axios.get<string>(encodeURI(url));
     return cheerio.load(body.data);
 }
