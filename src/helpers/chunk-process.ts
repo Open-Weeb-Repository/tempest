e21feec179dc60c174cfe266f31573af7d64e5e9
aquaswim <bayusetiawan37@gmail.com>
2020-08-14 19:23:11 +0700

    update chunk process to return array of process return

--- src/helpers/chunk-process.ts
+++ src/helpers/chunk-process.ts
@@ -1,10 +1,10 @@
 import chunk from "./chunk";
 
-export default async function chunkProcess <T, R = void>(items: T[], process: (item: T) => Promise<R>, nChunk = 1): Promise<R[][]> {
+export default async function chunkProcess <T, R = void>(items: T[], process: (item: T) => Promise<R>, nChunk = 1): Promise<R[]> {
     const chunkedItem = chunk<T>(items, nChunk);
-    const result: R[][] = [];
+    const result: R[] = [];
     for (let chunkedItemElement of chunkedItem) {
-        result.push(await Promise.all(chunkedItemElement.map(item => process(item))));
+        result.push(...await Promise.all(chunkedItemElement.map(item => process(item))));
     }
     return result;
 }
